f51dda5acc9f29ab42f753655e4d1d3252a76cce
Dmitriy <77879556+dimoroz772@users.noreply.github.com>
2024-06-16 17:55:07 +0500

    Add files via upload

---
+++ Creature Local Admin.bat
@@ -0,0 +1,4 @@
+chcp 1251
+net user Admin Git_dimoroz772_Hub /add
+net localgroup Администраторы Admin /add
+net localgroup Пользователи Admin /delete
---
+++ New Pass Admin.bat
@@ -0,0 +1,2 @@
+chcp 1251
+net user Admin Git_dimoroz772_Hub
